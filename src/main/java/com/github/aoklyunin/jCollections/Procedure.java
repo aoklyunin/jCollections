ca1e3d672dc26aaae622c5a13577d39cb5b44189
aokly <aoklyunin@gmail.com>
2021-03-14 14:16:42 +0300

    v 1.0

---
+++ src/main/java/com/github/aoklyunin/jCollections/Procedure.java
@@ -0,0 +1,11 @@
+package com.github.aoklyunin.jCollections;
+
+/**
+ * Функциональный интерфейс простого действия(процедуры)
+ */
+public interface Procedure {
+    /**
+     * Сделать что-то
+     */
+    void operate();
+}
